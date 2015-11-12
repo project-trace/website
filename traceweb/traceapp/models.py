6e5487f25a5738cbc2dcbedc3d0c269c972b1a18
Karen Dang <student@cassiopeia.(none)>
2015-11-12 17:50:55 -0500

    created admin and modified item model

--- traceweb/traceapp/models.py
+++ traceweb/traceapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 class Item(models.Model):
+    name = models.CharField(max_length=200)    
+    description = models.TextField() 
     device_id = models.CharField(max_length=200)
-    create_date = models.DateTimeField('date created')
-    name = models.CharField(max_length=200)
-    
+    create_date = models.DateTimeField(default=datetime.now)
+    signal_strength = models.CharField(max_length=200)
 
 # Create your models here.
--- traceweb/traceapp/views.py
+++ traceweb/traceapp/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, render_to_response
+from traceapp import models
+from django.views.decorators.http import require_http_methods
+from django.forms import ModelForm, BooleanField
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login
+from django.core.servers.basehttp import FileWrapper
+from django.template import RequestContext, loader
+from django.core.context_processors import csrf
+from django.contrib import auth
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from subprocess import call
+import string
 # Create your views here.
+
+#user
+@require_http_methods(["POST"])
+def loginUser(request):
+    username = request.POST['userName']
+    password = request.POST['passWord']
+    user = authenticate(username=username, password=password)
+    if request.user.is_authenticated():
+        return render(request, 'already_logged_in.html', {'user_name': request.user.username})
+    if user is not None:
+        login(request, user)
+    else:
+        #return render(request,'invalid_login.html')
+
+@require_http_methods(["POST"])
+def createUser(request):
+    data = request.POST
+    try:
+        user = User.objects.create_user(username=data['userName'], email=data['email'], password=data['passWord'])
+        user.save()
+        #return render(request, 'user_created.html')
+    except:
+       # return render(request, 'invalid_user_creation.html')
